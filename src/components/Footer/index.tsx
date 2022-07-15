be98d8dcb3cb1871ad868a6644b9a5bcfe0462cb
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2022-07-15 16:08:57 +0800

    feat: slogan

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   const intl = useIntl();
   const defaultMessage = intl.formatMessage({
     id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
+    defaultMessage: 'scnu',
   });
 
   const currentYear = new Date().getFullYear();
@@ -19,21 +19,15 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
+          key: '阅马后台管理系统',
+          title: '阅马后台管理系统',
           href: 'https://pro.ant.design',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          href: 'https://github.com/scnu-lib/scnu-lib-manage-fe',
           blankTarget: true,
         },
       ]}
